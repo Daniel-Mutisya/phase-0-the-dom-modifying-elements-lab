7f78eaccf80e8aa68b472f53796ff705cf1b1042
daniel <danielmutisya203@gmail.com>
2023-03-12 22:14:51 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+const element= document.getElementById("main");
+element.remove("main");
+const newHeader=document.createElement('h1');
+//creating h1 id
+newHeader.id="victory"
+//creating innerHTML inside h1
+newHeader.innerHTML="Dan is the champion";
